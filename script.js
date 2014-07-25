219c4c928f2200b1f07465fd7acb9b3d92865704
Andrew Swift <ac@acswift.com>
2014-07-25 17:32:57 +0200

    all working except scroller placement

--- script.js
+++ script.js
@@ -51,41 +51,17 @@
 
 	$('#noscroll').attr('style', stylestr);
 
-	throw new Error('end of js');
-
 //------------------------------------------------------- unfinished
 
 	// draw main scrolling content
-	content(offset_x + (content_x * scale), orig_height * scale);
-
-	fontsize = math.round(100*conversion);
-	$('body').css('font','normal '+fontsize+'px/'+fontsize+'px abel');
-
-	dimlogo   = ['#logo',  'width' ,350,'right',1500-380,'top',374];
-
-	dimombreg = ['#ombreg','height',667,'right',1500-410,'top',250 ];
-	dimombred = ['#ombred','height',667,'left' ,1410    ,'top',250 ];
-
-	dimombreh = ['#ombreh','width',1700,'left',-100,     'top' ,440];
-	dimombreb = ['#ombreb','width',1700,'left',-100,     'top' ,780];
-
-	dimmenu    = ['#menu   ','width',170,'right',1500-380,'top',525];
-	dimcontact = ['#contact','height',25,'right',1500-380,'top',815];
-
-	desslogo(dimlogo);
-	desslogo(dimombreg);
-	desslogo(dimombred);
-	desslogo(dimombreh);
-	desslogo(dimombreb);
-	desslogo(dimmenu);
-	desslogo(dimcontact);
+	content(offset_x + (offset_x * scale), orig_height * scale);
 
 //------------------------------------------------------- clicks on logo & accueil
 
 	$(document).ready(function() {
 		// if you click on welcome image it scrolls a little
 		$("#accueil").click(function(event){
-		    $('html, body').animate({scrollTop:workh/5}, 300);
+		    $('html, body').animate({scrollTop:100}, 300);
 		});
 
 		// if you click on logo it goes back to home page
